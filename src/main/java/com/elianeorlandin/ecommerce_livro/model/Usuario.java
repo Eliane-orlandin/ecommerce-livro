76f7db0f1cee3709e8964032d26795ae6d170a66
Eliane Orlandin <liorlandin33@gmail.com>
2026-06-05 15:05:49 -0300

    feat: adiciona model Usuario

---
+++ src/main/java/com/elianeorlandin/ecommerce_livro/model/Usuario.java
@@ -0,0 +1,103 @@
+package com.elianeorlandin.ecommerce_livro.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+
+	// ---- Atributos ----
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 100, nullable = false)
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
+	private String nome;
+	
+	
+	@Column(length = 100, nullable = false, unique = true)
+	@NotBlank(message = "O atributo usuário (email) é obrigatório")
+	@Email(message = "O usuário deve ser um email válido")
+	private String usuario;
+
+	@Column(length = 255, nullable = false)
+	@NotBlank(message = "A senha é obrigatória")
+	@Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
+	private String senha;
+
+	@Column(length = 255)
+	@Size(max = 255, message = "A URL da foto deve ter no máximo 255 caracteres")
+	private String foto;
+
+	// ---- Relacionamento entre tabelas ----
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties(value = "usuario", allowSetters = true)
+	private List<Livro> livro;
+
+	// ---- Getters e Setters -----
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public List<Livro> getLivro() {
+		return livro;
+	}
+
+	public void setLivro(List<Livro> livro) {
+		this.livro = livro;
+	}
+
+}
