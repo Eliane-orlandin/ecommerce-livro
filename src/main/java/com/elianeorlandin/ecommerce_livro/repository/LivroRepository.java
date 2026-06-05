85beb6603034ad02cc48f22838928bb3e0be6c91
Eliane Orlandin <liorlandin33@gmail.com>
2026-06-05 17:42:20 -0300

    feat: adiciona mais um método de busca por autor.

--- src/main/java/com/elianeorlandin/ecommerce_livro/repository/LivroRepository.java
+++ src/main/java/com/elianeorlandin/ecommerce_livro/repository/LivroRepository.java
@@ -3,11 +3,12 @@ package com.elianeorlandin.ecommerce_livro.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.elianeorlandin.ecommerce_livro.model.Livro;
 
 public interface LivroRepository extends JpaRepository<Livro, Long>{
 
-	List<Livro> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+	List<Livro> findAllByTituloContainingIgnoreCase(String titulo);
+	List<Livro> findAllByAutorContainingIgnoreCase(String autor);
+
 }
