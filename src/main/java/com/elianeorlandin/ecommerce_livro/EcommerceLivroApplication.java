1af08051bec8676f44c7589087a42aa2c1449571
Eliane Orlandin <liorlandin33@gmail.com>
2026-06-05 12:25:38 -0300

    chore: configuração inicial do projeto Spring Boot

---
+++ src/main/java/com/elianeorlandin/ecommerce_livro/EcommerceLivroApplication.java
@@ -0,0 +1,13 @@
+package com.elianeorlandin.ecommerce_livro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceLivroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elianeorlandin/ecommerce_livro/EcommerceLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elianeorlandin.ecommerce_livro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
